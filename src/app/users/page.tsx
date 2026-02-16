1503164ffcd5f93c60371115eab7e7fab02ecb66
israel.paixao <israeldouglas25@gmail.com>
2026-02-16 19:33:27 -0300

    personalisando a tela de cadastro

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,12 +3,51 @@ import Link from "next/link";
 import FormUsers from "../../components/FormUsers";
 
 export default function Cadastro() {
+  const handlerUsers = async (_: string, formData: FormData) => {
+    "use server";
+
+    const username = formData.get("username")?.toString();
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+
+    if (!username || !email || !password) {
+      return { message: "Preencha todos os campos", color: "bg-red-400" };
+    }
+
+    try {
+      const body = {
+        name: username,
+        email: email,
+        password: password
+      }
+
+      const res = await fetch("http://localhost:8080/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await res.json();
+
+      if (data.status) {
+        return { message: data.message || "Erro ao cadastrar usuário", color: "bg-red-400" };
+      }
+
+      return { message: data.message || "Usuário cadastrado com sucesso", color: "bg-green-400" };
+
+    } catch (error) {
+      console.error("handlerUsers failed:", error);
+      return { message: "Ocorreu um erro ao cadastrar o usuário", color: "bg-red-400" };
+    }
+  };
 
   return (
     <div className="grid gap-y-4 bg-white p-6 rounded-lg shadow-md">
       <h1 className="text-4xl font-bold text-center">Cadastro de Usuários</h1>
 
-      <FormUsers />
+      <FormUsers action={handlerUsers} />
 
       <Link className="text-center underline" href="/login">Já tenho cadastro</Link>
     </div>
--- src/components/FormUsers.tsx
+++ src/components/FormUsers.tsx
@@ -1,43 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { useActionState, useState } from "react";
 
-export default function FormUsers() {
+export default function FormUsers({ action }) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [response, formAction] = useActionState(action, null);
+
   return (
+    <>
+      {response && (
+        <p className={`px-4 py-2 ${response.color} text-white font-bold text-sm rounded-lg`}>
+          {response.message}
+          </p>
+        )}
 
-      <form action="" className="grid mt-4 mb-4 gap-y-2">
-        <input 
-        id="name" 
-        name="name" 
-        type="text" 
-        placeholder="Nome" 
-        value={username}
-        onChange={(e) => setUsername(e.target.value)} 
+      <form action={formAction} className="grid mt-4 mb-4 gap-y-2">
+        <input
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Nome"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
-        <input 
-        id="email" 
-        name="email" 
-        type="email" 
-        placeholder="Email" 
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} 
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        
-        <input 
-        id="password" 
-        name="password" 
-        type="password" 
-        placeholder="Senha" 
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} 
+
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button>Cadastrar</button>
       </form>
+    </>
   );
 }
