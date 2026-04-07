2117571baeb8488d1f6c17ab8f98d3f6123e4d58
israelpaixao <israeldouglas25@gmail.com>
2026-04-07 20:56:41 -0300

    ajustando formulario de produtos

--- src/components/FormProduct.tsx
+++ src/components/FormProduct.tsx
@@ -1,37 +1,59 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { FC, useActionState, useState } from "react";
+import { FC, useActionState, useState } from 'react';
 
-import { FormInput } from "./FormInput";
-import { FormButton } from "./FormButton";
-import { FormResponse } from "./FormResponse";
+import { FormInput } from './FormInput';
+import { FormButton } from './FormButton';
+import { FormResponse } from './FormResponse';
 
 type FormProductProps = {
-  action: (_: string, formData: FormData) => Promise<any>;
+  action: (
+    state: { message: string; color: string } | null,
+    formData: FormData
+  ) => Promise<{ message: string; color: string } | null>;
 };
 
 export const FormProduct: FC<FormProductProps> = ({ action }) => {
-  const [name, setName] = useState("");
-  const [stockQuantity, setStockQuantity] = useState("");
-  const [categoryId, setCategoryId] = useState("");
-  const [unitValue, setUnitValue] = useState("");
+  const [name, setName] = useState('');
+  const [stockQuantity, setStockQuantity] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [unitValue, setUnitValue] = useState('');
 
   const [response, formAction] = useActionState(action, null);
-
-  // obtain router instance once inside component body
-  const router = useRouter();
 
   return (
     <>
       <FormResponse response={response} />
 
       <form action={formAction} className="grid mt-4 mb-4 gap-y-2">
-
-        <FormInput id="name" type="text" placeholder="Nome" value={name} setValue={setName} />
-        <FormInput id="stockQuantity" type="number" placeholder="Quantidade em estoque" value={stockQuantity} setValue={setStockQuantity} />
-        <FormInput id="categoryId" type="number" placeholder="ID da categoria" value={categoryId} setValue={setCategoryId} />
-        <FormInput id="unitValue" type="number" placeholder="Valor unitário (R$)" value={unitValue} setValue={setUnitValue} />
+        <FormInput
+          id="name"
+          type="text"
+          placeholder="Nome"
+          value={name}
+          setValue={setName}
+        />
+        <FormInput
+          id="stockQuantity"
+          type="number"
+          placeholder="Quantidade em estoque"
+          value={stockQuantity}
+          setValue={setStockQuantity}
+        />
+        <FormInput
+          id="categoryId"
+          type="number"
+          placeholder="ID da categoria"
+          value={categoryId}
+          setValue={setCategoryId}
+        />
+        <FormInput
+          id="unitValue"
+          type="number"
+          placeholder="Valor unitário (R$)"
+          value={unitValue}
+          setValue={setUnitValue}
+        />
 
         <FormButton className="bg-orange-500 text-white hover:bg-orange-600 font-bold">
           Cadastrar
@@ -39,4 +61,4 @@ export const FormProduct: FC<FormProductProps> = ({ action }) => {
       </form>
     </>
   );
-}
+};
